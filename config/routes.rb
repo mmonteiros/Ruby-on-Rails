a35e517f95f669078cdf0bdcd03ea7522a82a464
Mateus Monteiro <mateusmonteiroasa@gmail.com>
2021-10-13 11:23:03 -0300

    Updates

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+
+  get "contact", to: "pages#contact"
+  get "about", to: "pages#about"
+  get "error", to: "pages#error"
+
+  get "blog", to: redirect("http://jordanhudgens.com")
+
+  resources :projects
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+
+  get "*path", to: redirect("/error")
+end
