024d10b93d645ecb58a1c65f9510f02d7f35ec14
Mateus Monteiro <mateusmonteiroasa@gmail.com>
2021-10-15 12:45:21 -0300

    Update fog

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,10 +1,19 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
 
     config.fog_credentials = {
       provider:              'AWS',                            # required
       aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],            # required
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],            # required
+      region:                'sa-east-1', 
     }
     config.fog_directory = ENV["AWS_BUCKET"]                   # required
     config.fog_public    = false     # To let CarrierWave work on Heroku
+    if Rails.env.production? 
+      config.fog_directory = ENV["PRODUCTION_S3_BUCKET"] 
+    else config.fog_directory = ENV["DEVELOPMENT_S3_BUCKET"] 
+    end
   end
